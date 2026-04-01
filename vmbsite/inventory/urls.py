0eb337a9f17b8e6a3d5d9d3ba68464a837d5a69c
Jake Gendreau <jake.lgendreau@gmail.com>
2026-04-01 11:48:53 -0700

    Added a bunch of frontends

--- vmbsite/inventory/urls.py
+++ vmbsite/inventory/urls.py
@@ -4,4 +4,12 @@ from . import views
 app_name = "vmb"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("signup/", views.signup, name="signup"),
+    path("instrument/", views.instrument, name="instrument"),
+    path("uniform/", views.uniform, name="uniform"),
+    path("database/", views.database, name="database"),
+    path("database/member_db/", views.member_db, name="member_db"),
+    path("database/uniform_db/", views.uniform_db, name="uniform_db"),
+    path("database/instrument_db/", views.instrument_db, name="instrument_db"),
 ]
--- vmbsite/inventory/views.py
+++ vmbsite/inventory/views.py
@@ -2,4 +2,52 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Index!")
+    return render(request, "vmbsite/index.html")
+
+def login(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/login.html")
+
+def signup(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/signup.html")
+
+def instrument(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/instrument.html")
+
+def uniform(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/uniform.html")
+
+def database(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/database.html")
+
+def member_db(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/member_db.html")
+
+def uniform_db(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/uniform_db.html")
+
+def instrument_db(request):
+    if(request.method == 'POST'):
+        pass
+
+    return render(request, "vmbsite/instrument_db.html")
